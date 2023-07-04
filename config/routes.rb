bef02e49c77c80ea539c740422936a068e6be5b2
TEnZII843 <karma.tenzii7@gmail.com>
2023-07-04 11:25:37 +0600

    feat: setting up project

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root "articles#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  # get "/articles/new", to: "articles#new"
+
+  # resources :article, only: ['show', 'index']
+
+  # get '/articles/download', to: 'articles#download', as: :article_download
+
+  resources :articles
+end
